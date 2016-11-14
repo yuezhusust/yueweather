5226b069511d05b2c981631ff54247eaacfd20df
yuezhu <40314525@qq.com>
2016-11-14 17:53:28 +0800

    第一次提交悦weather

---
+++ app/src/main/java/com/yueweather/app/MainActivity.java
@@ -0,0 +1,12 @@
+package com.yueweather.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
